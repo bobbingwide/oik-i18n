1ffeb1a7191f66ea1895a03dc090fa5b39196eba
Herb Miller <herb@bobbingwide.com>
2016-08-02 10:48:11 +0100

    Fixes #2 - recreate bb_BB version of WordPress language files

--- bb_king.php
+++ bb_king.php
@@ -1,7 +1,7 @@
 <?php // (C) Copyright Bobbing Wide 2016
 
 /** 
- * Syntax oikwp bb_king.php
+ * Syntax: oikwp bb_king.php
  * run from the oik-i18n directory
  * 
  * Create the bb_BB.po and .mo files from the English language version files for WordPress
@@ -10,58 +10,33 @@
  * - Update WordPress to the latest level
  * - Update the translations for the English language version
  * - Run bb_king.php
- 
- * Note: bb_BB.php needs to work off .po files as well as .pot files
  *
+ * Note: bb_BB.php needs to work off .po files as well as .pot files, which means
+ * - bb_BB has to handle msgstr values which have already been set.
+ * - bb_BB has to know which files are la_CY.po files (with locale) and which are .pot ( without )
+ * - bb_BB has to change directory to WP_LANG_DIR to locate these files
+ */
  
- 
-
-cd \apache\htdocs\wpms\wp-content\languages
-
-php \apache\htdocs\wordpress\wp-content\plugins\oik-i18n\bb_BB.php admin-en_GB.po 
-
-
-
-php bb_BB.php admin-network-en_GB.po
-php bb_BB.php en_GB.po
-
-
- Directory of c:\apache\htdocs\wpms\wp-content\languages
-
-01/08/2016  21:52           332,749 admin-en_GB.mo
-01/08/2016  21:52           464,074 admin-en_GB.po
-01/08/2016  21:52            46,044 admin-network-en_GB.mo
-01/08/2016  21:52            62,558 admin-network-en_GB.po
-01/08/2016  21:52           158,141 en_GB.mo
-01/08/2016  21:52           267,930 en_GB.po
-
-*/
-
+/**
+ * Function to invoke when loaded 
+ */ 
 function bb_king_loaded() {
-
 	oik_require( "bb_BB.php", "oik-i18n" );
-	//bb_BB( "admin-en_GB" );
-	//bb_BB( "admin-network" );
-	
 	bb_BB( "admin-" );
 	bb_BB( "admin-network-" );
 	bb_BB( "" );
-	
 	do_msgfmt( "admin-" );
 	do_msgfmt( "admin-network-" );
 	do_msgfmt( "" );
-	
-	
-
 }
-
 
 /**
  * Create a .mo file from a .po file 
  * 
  * Invoke the msgfmt program to convert the locale's .po file to the .mo file
+ * Note: this is a local version that caters for WordPress's global files
  * 
- * @param string $plugin - the plugin slug e.g. oik-privacy-policy
+ * @param string $plugin - the plugin slug - may be blank
  * @param string $locale - the target locale
  * 
  */
@@ -72,6 +47,5 @@ function do_msgfmt( $plugin, $locale="bb_BB" ) {
   echo "$res $text" . PHP_EOL;
   return( $res );
 } 
-
 
 bb_king_loaded();
